1935215864d8a28ef193652f3425a3af4408da9a
olga-plotkina <olga.semenyuta@gmail.com>
2022-11-03 17:06:43 +0000

    add searchbox component

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,46 @@
+import * as Yup from 'yup';
+import { Formik, ErrorMessage } from 'formik';
+import {
+  SearchForm,
+  StyledSearchbar,
+  SearchFormButton,
+  SearchLabel,
+  SearchInput,
+} from './Searchbar.styled';
+
+const schema = Yup.string().required;
+
+const initialValues = {
+  search: '',
+};
+
+export const Searchbar = ({ submitProp }) => {
+  const handleSubmit = (values, { resetForm }) => {
+    submitProp(values);
+    resetForm();
+  };
+  return (
+    <StyledSearchbar>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={schema}
+      >
+        <SearchForm>
+          <SearchFormButton type="submit">
+            <SearchLabel>Search</SearchLabel>
+          </SearchFormButton>
+
+          <SearchInput
+            name="search"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+          <ErrorMessage name="search" component="div" />
+        </SearchForm>
+      </Formik>
+    </StyledSearchbar>
+  );
+};
