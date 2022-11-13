e201a56a26a9e7a3b469d89dd8b16701b8769e00
olga-plotkina <olga.semenyuta@gmail.com>
2022-11-13 18:14:38 +0000

    refactoring of ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,36 +1,30 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import { useState } from 'react';
 import { Modal } from 'components/Modal';
 import { GalleryItem, ItemImage } from './ImageGalleryItem.styled';
 
-export class ImageGalleryItem extends React.Component {
-  static propTypes = {
-    onClick: PropTypes.func,
-    preview: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    onClose: PropTypes.func,
+export function ImageGalleryItem() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
-  state = { isModalOpen: false };
-
-  openModal = () => {
-    this.setState({ isModalOpen: true });
-  };
-
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
-  };
-
-  render() {
-    return (
-      <GalleryItem onClick={this.openModal}>
-        <ItemImage src={this.props.preview} alt={this.props.description} />
-        {this.state.isModalOpen && (
-          <Modal onClose={this.closeModal}>
-            <img src={this.props.bigImage} alt={this.props.description} />
-          </Modal>
-        )}
-      </GalleryItem>
-    );
-  }
+  return (
+    <GalleryItem onClick={toggleModal}>
+      <ItemImage src={this.props.preview} alt={this.props.description} />
+      {isModalOpen && (
+        <Modal onClose={toggleModal}>
+          <img src={this.props.bigImage} alt={this.props.description} />
+        </Modal>
+      )}
+    </GalleryItem>
+  );
 }
+
+ImageGalleryItem.propTypes = {
+  onClick: PropTypes.func,
+  preview: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  onClose: PropTypes.func,
+};
